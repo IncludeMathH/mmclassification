46328dfd087d5513eb00366c0f18144ad7c58e09
IncludeMathH <798511305@qq.com>
2022-10-26 21:01:50 +0800

    fine-tune on ViT model

---
+++ configs/meituan/vit-base-p16_ft-4xb64_meituan-384.py
@@ -0,0 +1,65 @@
+_base_ = [
+    '../_base_/models/vit-base-p16.py',
+    '../_base_/datasets/meituan_bs64_pil_resize_autoaug.py',
+    '../_base_/schedules/imagenet_bs4096_AdamW.py',
+    '../_base_/default_runtime.py'
+]
+
+# 1. model config file
+model = dict(
+    backbone=dict(
+        img_size=384,
+        init_cfg=dict(
+            _delete_=True,
+            type='Pretrained',
+            checkpoint='https://download.openmmlab.com/mmclassification/v0/vit/finetune/vit-base-p16_in21k-pre-3rdparty_ft-64xb64_in1k-384_20210928-98e8652b.pth',
+            prefix='backbone',
+        )
+    ))
+
+# 2. datasets config file
+img_norm_cfg = dict(
+    mean=[127.5, 127.5, 127.5], std=[127.5, 127.5, 127.5], to_rgb=True)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='RandomResizedCrop', size=384, backend='pillow'),
+    dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='ToTensor', keys=['gt_label']),
+    dict(type='Collect', keys=['img', 'gt_label'])
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', size=(384, -1), backend='pillow'),
+    dict(type='CenterCrop', crop_size=384),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='Collect', keys=['img'])
+]
+
+data = dict(
+    train=dict(pipeline=train_pipeline),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline),
+)
+
+# 3. shedule config file
+# optimizer. original lr=0.003, batch size = 64*64
+optimizer = dict(
+    lr=0.0003 * 4*64/(64*64),
+)
+runner = dict(type='EpochBasedRunner', max_epochs=100)
+
+# 4. runtime file
+# checkpoint saving
+checkpoint_config = dict(interval=5)
+# yapf:disable
+log_config = dict(
+    interval=20,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
